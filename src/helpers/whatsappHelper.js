38a350a8d38c8001e5065a41a8c52cbf8abddeee
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-06-25 14:18:32 -0400

    etilos

--- src/helpers/whatsappHelper.js
+++ src/helpers/whatsappHelper.js
@@ -1,6 +1,9 @@
 const twilio = require("twilio");
 
-const client = twilio(process.env.TWILIO_ACCOUNT_SID, process.env.TWILIO_AUTH_TOKEN);
+const client = twilio(
+  process.env.TWILIO_ACCOUNT_SID,
+  process.env.TWILIO_AUTH_TOKEN
+);
 const FROM = process.env.TWILIO_WHATSAPP_FROM;
 
 /**
