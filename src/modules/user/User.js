7af94d42d05ab5f7093a5c3515b806e867536271
Wilmer Casilimas Sanchez <wilmercasilimas@gmail.com>
2025-07-25 19:36:15 -0400

    Agrega campo 'oculto' al modelo de usuario para soportar doctor marcador

--- src/modules/user/User.js
+++ src/modules/user/User.js
@@ -69,6 +69,10 @@ const userSchema = new mongoose.Schema(
       type: Boolean,
       default: false,
     },
+    oculto: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     timestamps: true, // createdAt y updatedAt
